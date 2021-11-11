09063058487fc14bdb709217c5ef419c49b5171c
Balaji Munusamy <38127049+balaji-munusamy@users.noreply.github.com>
2021-11-11 21:21:21 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,4 +1,4 @@
 curl -o python-installer.bat https://raw.githubusercontent.com/balaji-munusamy/va-starter/main/python-installer.bat
-curl -o modules-installer.bat https://raw.githubusercontent.com/balaji-munusamy/va-starter/main/modules-installer.bat
 call python-installer.bat
-call modules-installer.bat
+curl -o modules-installer.bat https://raw.githubusercontent.com/balaji-munusamy/va-starter/main/modules-installer.bat
+modules-installer.bat
