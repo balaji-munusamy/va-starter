5d3fd4d6b1be6b5a4c95ab82ea5a6887621fab71
Balaji Munusamy <38127049+balaji-munusamy@users.noreply.github.com>
2021-11-11 21:03:51 +0530

    Create installer.bat

---
+++ installer.bat
@@ -0,0 +1,4 @@
+curl -o python-installer.bat https://raw.githubusercontent.com/balaji-munusamy/va-starter/main/python-installer.bat
+curl -o modules-installer.bat https://github.com/balaji-munusamy/va-starter/blob/main/modules-installer.bat
+call python-installer.bat
+call modules-installer.bat
